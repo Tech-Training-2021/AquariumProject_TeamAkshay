c0f4c1158ba930dd98d34dcdd1b490b6f17ca184
akshay9967 <akshay3708@gmail.com>
2021-08-18 20:42:52 +0530

    Added mumbai store location

--- FishStoreConsole/Program.cs
+++ FishStoreConsole/Program.cs
@@ -5,6 +5,8 @@ using System.Collections;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using ConsoleTables;
 
 namespace FishStoreConsole
@@ -23,8 +25,12 @@ namespace FishStoreConsole
             int chooseoption = int.Parse(Console.ReadLine());
              switch (chooseoption)
              {
-                 case 1: Menu(); break;
-                case 2:  break;
+                 case 1: 
+                    Menu(); 
+                    break;
+                 case 2:
+                    chooseLocation();
+                    break;
              }
         }
 
@@ -169,8 +175,158 @@ namespace FishStoreConsole
 
               return tank;
 
-          } */ 
+          } */
         #endregion
+
+
+        public static void chooseLocation()
+        {
+            Console.Write("1.Mumbai\n2.Delhi\n3.Bangalore\nChoose one option :   ");
+            int chooseoption = int.Parse(Console.ReadLine());
+            switch (chooseoption)
+            {
+                case 1:
+                    productsFromMumbai();
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+
+        }
+
+        public static void productsFromMumbai()
+        {
+            //string filePath = @"C:\dotnet\Project\FishStoreLib\FishStoreConsole\mumbaiData.json";
+            //string fishJson=File.ReadAllText(filePath);
+
+            //var jObject = JObject.Parse(fishJson);
+            //var fishDataArray = jObject.GetValue("fishData") as JArray;
+            //var fishFoodArray = jObject.GetValue("fishFood") as JArray;
+            //var fishTankArray = jObject.GetValue("fishTank") as JArray;
+            //Console.WriteLine(fishDataArray);
+
+            Mumbai mu = new Mumbai();
+            mu.fetchDetails();
+            //Console.WriteLine(mu.fishDataArray);
+            menuWithinLocation(mu);
+
+
+
+
+        }
+
+        public static void menuWithinLocation(Mumbai mu)
+        {
+            Console.Write("1.Buy pet fish\n2.Buy fish food\n3.Buy tank\nChoose one option :   ");
+            int chooseoption = int.Parse(Console.ReadLine());
+            switch (chooseoption)
+            {
+                case 1:
+                    FishFromMumbai(mu.fishDataArray, mu);
+                    break;
+                case 2:
+                    FoodFromMumbai(mu.fishFoodArray, mu);
+                    break;
+                case 3:
+                    TankFromMumbai(mu.fishTankArray, mu);
+                    break;
+                default:
+                    chooseLocation();
+                    break;
+            }
+        }
+        public static void FishFromMumbai(JArray fishDataArray, Mumbai mu)
+        {
+            string type = "fish";
+
+
+            var table = new ConsoleTable("Id", "Fish Name", "Price", "Available Quantity");
+            foreach (var x in fishDataArray)
+            {
+                table.AddRow(x["id"], x["Name"], x["Price"], x["availableQuantity"]);
+            }
+            table.Write();
+            Console.Write("Choose id to buy fish : ");
+            int selectedId = int.Parse(Console.ReadLine());
+            foreach (var y in fishDataArray)
+            {
+                int id = Convert.ToInt32(y["id"]);
+
+                if (id == selectedId)
+                {
+                    Console.WriteLine($"You bought {y["Name"]} which cost you {y["Price"]}");
+                    int updatedAvailableQuantity = Convert.ToInt32(y["availableQuantity"]) - 1;
+                    Console.WriteLine("Updated Quantity " + updatedAvailableQuantity);
+                    mu.saveData(updatedAvailableQuantity, selectedId, type);
+                    ContinueOrExitLocation(mu);
+                    break;
+                }
+            }
+
+        }
+
+        public static void FoodFromMumbai(JArray foodDataArray, Mumbai mu)
+        {
+            string type = "food";
+            var table = new ConsoleTable("id", "Food", "Price", "Available Quantity");
+            foreach (var x in foodDataArray)
+            {
+                table.AddRow(x["id"], x["food"], x["Price"], x["availableQuantity"]);
+            }
+            table.Write();
+            Console.Write("Choose id to buy food : ");
+            int selectedId = int.Parse(Console.ReadLine());
+            foreach (var y in foodDataArray)
+            {
+                int id = Convert.ToInt32(y["id"]);
+
+                if (id == selectedId)
+                {
+                    Console.WriteLine($"You bought {y["food"]} which cost you {y["Price"]}");
+                    int updatedAvailableQuantity = Convert.ToInt32(y["availableQuantity"]) - 1;
+                    //Console.WriteLine("Updated Quantity " + updatedAvailableQuantity);
+                    mu.saveData(updatedAvailableQuantity, selectedId, type);
+                    ContinueOrExitLocation(mu);
+                    break;
+                }
+            }
+
+
+        }
+
+
+        public static void TankFromMumbai(JArray tankDataArray, Mumbai mu)
+        {
+            string type = "tank";
+
+            var table = new ConsoleTable("id", "name", "price", "Available Quantity");
+            foreach (var x in tankDataArray)
+            {
+                table.AddRow(x["id"], x["Name"], x["Price"], x["availableQuantity"]);
+            }
+            table.Write();
+            Console.Write("Choose id to buy tank : ");
+            int selectedId = int.Parse(Console.ReadLine());
+            foreach (var y in tankDataArray)
+            {
+                int id = Convert.ToInt32(y["id"]);
+
+                if (id == selectedId)
+                {
+                    Console.WriteLine($"You bought {y["Name"]} which cost you {y["Price"]}");
+                    int updatedAvailableQuantity = Convert.ToInt32(y["availableQuantity"]) - 1;
+                    //Console.WriteLine("Updated Quantity " + updatedAvailableQuantity);
+                    mu.saveData(updatedAvailableQuantity, selectedId, type);
+                    ContinueOrExitLocation(mu);
+                    break;
+                }
+            }
+
+
+        }
+
+
 
 
 
@@ -236,7 +392,14 @@ namespace FishStoreConsole
             if (result == "y" || result == "Y") Menu();
             else Main();
         }
-        
+
+        static void ContinueOrExitLocation(Mumbai mu)
+        {
+            Console.WriteLine("Do you want to continue? y/n");
+            var result = Console.ReadLine();
+            if (result == "y" || result == "Y") menuWithinLocation(mu);
+            else chooseLocation();
+        }
 
     }
 }
